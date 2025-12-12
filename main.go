24e181866cda2fbede5fe38214fff0443b42bbb9
blu-Encelado <bluencelado@gmail.com>
2025-12-12 12:08:54 +0100

    Changes

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	//fmt.Println("===Pokedex===")
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		fmt.Print("Your command was: ", cleanInput(scanner.Text())[0], "\n")
 
-	}
+	startRepl()
 
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+func startRepl() {
+	//fmt.Println("===Pokedex===")
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		reader.Scan()
+
+		words := cleanInput((reader.Text()))
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := GetCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknow command")
+		}
+
+	}
+}
 func cleanInput(text string) []string {
 	newText := strings.Fields(strings.TrimSpace(strings.ToLower(text)))
 
